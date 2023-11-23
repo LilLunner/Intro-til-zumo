e40f724a83ae7d27fb7eb859ac248a2637ec6b38
LilLunner <magnus.frafjord@lyse.net>
2023-11-23 11:08:03 +0100

    f

--- Knapper/Knapper.ino
+++ Knapper/Knapper.ino
@@ -1,8 +1,16 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
 
+Zumo32U4ButtonA buttonA;
+Zumo32U4ButtonB buttonB;
+Zumo32U4ButtonC buttonC;
 
 void setup() {
-
+    display.clear();
+    display.print(F("Press a button to start"));
 }
+
+
 
 void loop() {
 
