33c8ecfb9a743b550d3ca5b4e8a1a69eebaea764
ellandoian <jjojjo3214@gmail.com>
2023-12-11 14:03:27 +0100

    linefollow PID funker ikke

--- Linefollower/Linefollower.ino
+++ Linefollower/Linefollower.ino
@@ -1,39 +1,61 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
-
 Zumo32U4LineSensors lineSensors;
 Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4IMU imu;
+Zumo32U4OLED display;
+#include "TurnSensor.h"
 
-const uint16_t maxSpeed = 400;
-int16_t lastError = 0;
+int topSpeed = 200;
 
-#define NUM_SENSORS 5
-unsigned int lineSensorValues[NUM_SENSORS];
+void turnDeg(int x, int y) // x er antal rotasjoner, y er vinkeel
+{
+    int i = 1;
+    while (i <= x) // ekke helt sikker på hvofor denne  er nødvendig, men funker ikke uten
+    {
+        motors.setSpeeds(-100, 100);
+        while ((int32_t)turnAngle < (turnAngle1 * (y))) // stopper her helt til den har truffet 90*, det virker som hva som hva som er 90 endres fra dag til dag
+        {
+            turnSensorUpdate();
+        }
+        turnSensorReset(); // reseter gyroskop dataen
+        motors.setSpeeds(0, 0);
+        i++;
+    }
+}
 
-void calibrate() {
-
+void lineCalibrate()
+{
+    turnDeg(4, 90);
     lineSensors.calibrate();
 }
 
-void lineDrive() {
-    int position = lineSensors.readLine(lineSensorValues);
-    int error = position-2000;
-    int speedDifference = error / 4 + 6 * (error - lastError);
-    lastError = error;
-    int16_t leftSpeed = (int16_t)maxSpeed + speedDifference;
-    int16_t rightSpeed = (int16_t)maxSpeed - speedDifference;
-
-    leftSpeed = constrain(leftSpeed, 0, (int16_t)maxSpeed);
-    rightSpeed = constrain(rightSpeed, 0, (int16_t)maxSpeed);
-
-    motors.setSpeeds(leftSpeed, rightSpeed);
-
+void setup()
+{
+    turnSensorSetup();
+    lineCalibrate();
 }
 
-void setup() {
-    lineSensors.initFiveSensors();
+int lineSensorRead()
+{
+    static int prevError;
+    static unsigned int lineSensorVal[5];
+    int error = map(lineSensors.readLine(lineSensorVal), 0, 4000, -2000, 2000);
+    int correction = error/4+6*(error-prevError); //kilde eksempelkode
+    prevError = error;
+    return correction;
 }
 
-void loop() {
+void lineFollow(int correction){
+    int lSpeed = topSpeed + correction;
+    int rSpeed = topSpeed - correction;
+    lSpeed = constrain(lSpeed, 0, topSpeed);
+    rSpeed = constrain(rSpeed, 0, topSpeed);
+    motors.setSpeeds(lSpeed, rSpeed);
+}
 
+void loop()
+{
+    lineFollow(lineSensorRead());
 }
