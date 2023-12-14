ff8baed5c8258eb05947d41fbcad3dea01076954
LilLunner <magnus.frafjord@lyse.net>
2023-12-14 23:00:20 +0100

    moenster

--- moensterOgSens/moensterOgSens.ino
+++ moensterOgSens/moensterOgSens.ino
@@ -4,6 +4,7 @@
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4ButtonB buttonB;
+Zumo32U4ButtonC buttonC;
 Zumo32U4OLED display;
 Zumo32U4IMU imu;
 Zumo32U4LineSensors lineSensors;
@@ -123,6 +124,29 @@ void showProxA()
         }
         i++;
     }
+}
+
+void pressA() {
+    if (buttonA.isPressed())
+        square();
+}
+
+void pressB() {
+    if (buttonB.isPressed())
+        circle();
+}
+
+void pressC() {
+    if (buttonC.isPressed())
+        zigzag();
+}
+
+void Interface() {
+    display.print("Press A for square.");
+    display.gotoXY(0,2);
+    display.print("Press B for circle.");
+    display.gotoXY(0,4);
+    display.print("Press C for slalom.");
 }
 
 void setup()
